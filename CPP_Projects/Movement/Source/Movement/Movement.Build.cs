dfdb98ca0f01c90ed855cc69e0e9b8d3306ee0ac
Michael Geffre <geffrejunior92@gmail.com>
2020-06-24 16:26:19 -0700

    Initial Commit with basic movement

---
+++ CPP_Projects/Movement/Source/Movement/Movement.Build.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Movement : ModuleRules
+{
+	public Movement(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
